2d452ed24585549a50d91328d83c566a888d7f99
lucasgsfelix <lucasgsfelix@gmail.com>
2018-06-07 11:32:37 -0300

    Add files via upload

--- modelagemTopicos.py
+++ modelagemTopicos.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
 import gensim
+import re
 from gensim import corpora
 
 ##### esse código será responsável por fazer a modelagem de tópicos no nosso trabalho
@@ -30,26 +31,64 @@ def preProcessamento(info): ### pré processando os dados
 	
 	stop_free = " ".join([i for i in info.lower().split() if i not in stop]) ## retirando stop words
 	punc_free = ''.join(ch for ch in stop_free if ch not in exclude) ### retirando pontuação
-	normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split()) ### normalizando
 
-	return normalized
+	return punc_free
+
+
+def removeWords(adjectives, text):
+	
+	i=0
+	while(i<len(text)):
+
+		j=0
+		while(j<len(text[i])):
+
+			if len([(a.end()) for a in list(re.finditer(text[i][j], adjectives))]) != 0:
+
+				text[i].pop(j)
+				j=j-1
+
+			j=j+1
+
+			if len(text[i]) == 0:
+				break
+
+		if len(text) == 0:
+			break
+
+		i=i+1
+
+	return text
+
+def readWords(file_name):
+
+	text_file = open(file_name, 'r')
+	info = text_file.read()
+	
+	text_file.close()
+
+	return info
 
 if __name__ == '__main__':
 	
 	info = leitura("reclameAqui.txt", 6)
 	stop = set(stopwords.words('portuguese'))
 	exclude = set(string.punctuation) 
-	lemma = WordNetLemmatizer()
 
 	for i in range(0, len(info)):
 		### o que eu preciso retirar <br
 		info[i] = info[i].replace('<br', ' ')
 
 	info = [preProcessamento(data).split() for data in info] ## ao final tenho tudo pre processado
+	adjectives = readWords('lista_adjetivos')
+	stop = readWords('stop_words')
+	info = removeWords(adjectives, info) ### retirando adjetivos
+	info = removeWords(stop, info) ### retirando stop words
 
+
+	#### parte de modelagem de tópicos
 	dictionary = corpora.Dictionary(info)
 	doc_term_matrix = [dictionary.doc2bow(doc) for doc in info]
-
 	Lda = gensim.models.ldamodel.LdaModel
 	ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
 
